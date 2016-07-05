643001c27a527f5df9c54cf7feb9624538a863f5
Anthony <martab7.am@gmail.com>
2016-07-05 10:35:33 -0500

    Adds the capability for user to input arguments for min and max

--- High_Low_Game.php
+++ High_Low_Game.php
@@ -1,17 +1,23 @@
 <?php
 
-$randomNumber = mt_rand(0, 100);
+$randomNumber = mt_rand($argv[1], $argv[2]);
 
 do{
   fwrite(STDOUT, 'Guess my number! ');
   $guess = fgets(STDIN);
 
-  if($guess < $randomNumber) {
-    fwrite(STDOUT, "The answer is greater than $guess\n");
-  } elseif ($guess > $randomNumber) {
-    fwrite(STDOUT, "The answer is less than $guess\n");
+  if($guess < $argv[1]) {
+    fwrite(STDOUT, "$guess is less than the minimum. Please select a number that is greater than {$argv[1]}");
+  } elseif($guess > $argv[2]) {
+    fwrite(STDOUT, "$guess is higher than the max. Please select a number that is greater than {$argv[2]}");
   } else {
-    fwrite(STDOUT, "Correct\! \n");
-    exit(0);
-  }
+    if($guess < $randomNumber) {
+      fwrite(STDOUT, "The answer is greater than $guess\n");
+    } elseif ($guess > $randomNumber) {
+      fwrite(STDOUT, "The answer is less than $guess\n");
+    } else {
+      fwrite(STDOUT, "Correct\! \n");
+      exit(0);
+    }
+}
 } while($guess !== $randomNumber);
