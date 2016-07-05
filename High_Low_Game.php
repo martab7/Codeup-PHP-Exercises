8b69a7aa7f7c1b4efb358e4bc2b88461476c79dc
Anthony <martab7.am@gmail.com>
2016-07-05 10:53:38 -0500

    Updates high/low game with cleaner argument input functionality

--- High_Low_Game.php
+++ High_Low_Game.php
@@ -1,15 +1,28 @@
 <?php
 
-$randomNumber = mt_rand($argv[1], $argv[2]);
+$min = 1;
+$max = 100;
+
+if($argc === 3) {
+  if(is_numeric($argv[1]) && is_numeric($argv[2])) {
+    $min = $argv[1];
+    $max = $argv[2];
+  } else {
+      fwrite(STDERR, "You need to pass 2 numeric values\n");
+      exit(1);
+  }
+}
+
+$randomNumber = mt_rand($min, $max);
 
 do{
   fwrite(STDOUT, 'Guess my number! ');
   $guess = fgets(STDIN);
 
   if($guess < $argv[1]) {
-    fwrite(STDOUT, "$guess is less than the minimum. Please select a number that is greater than {$argv[1]}");
+    fwrite(STDOUT, "$guess is less than the minimum. Please select a number that is greater than {$argv[1]}\n");
   } elseif($guess > $argv[2]) {
-    fwrite(STDOUT, "$guess is higher than the max. Please select a number that is greater than {$argv[2]}");
+    fwrite(STDOUT, "$guess is higher than the max. Please select a number that is greater than {$argv[2]}\n");
   } else {
     if($guess < $randomNumber) {
       fwrite(STDOUT, "The answer is greater than $guess\n");
