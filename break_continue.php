1c0551860ecf083b658be71868ed906ccbc9ed95
Anthony <martab7.am@gmail.com>
2016-07-06 10:01:34 -0500

    Adds exercise for break/continue functions

---
+++ break_continue.php
@@ -0,0 +1,15 @@
+<?php
+
+foreach(range(1, 100) as $i) {
+  if($i % 2 === 1){
+    continue;
+  };
+  echo "{$i}\n";
+}
+
+foreach(range(1, 100) as $i) {
+  echo "{$i}\n";
+  if ($i == 10) {
+    break;
+  }
+}
