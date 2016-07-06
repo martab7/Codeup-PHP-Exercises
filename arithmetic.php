287c34da7ff45416efa528a64114c1e68280c4bb
Anthony <martab7.am@gmail.com>
2016-07-06 10:54:28 -0500

    Adds exercise for php functions

---
+++ arithmetic.php
@@ -0,0 +1,31 @@
+<?php
+
+$a = 2;
+$b = 5;
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b;
+}
+
+function multiply($a, $b)
+{
+    return $a * $b;
+}
+
+function divide($a, $b)
+{
+    return $a / $b;
+}
+
+function modulus($a, $b)
+{
+    return $a % $b;
+}
+
+// Add code to test your functions here
