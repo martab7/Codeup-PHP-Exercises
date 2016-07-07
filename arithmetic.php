ee651eba3547c3695c62b34a7fd4696c5d44c913
Anthony <martab7.am@gmail.com>
2016-07-07 15:14:37 -0500

    Adds arithmetic file for more work on functions and nesting

--- arithmetic.php
+++ arithmetic.php
@@ -1,7 +1,39 @@
 <?php
 
-$a = 2;
-$b = 5;
+$a = $argv[1];
+$b = $argv[2];
+
+
+function validate($a, $b) {
+  if(is_numeric($a) && is_numeric($b)) {
+    return true;
+  }
+  if(!is_numeric($a)) {
+    fwrite(STDERR, "$a should be a number.\n");
+    return false;
+    exit(1);
+  }
+  if(!is_numeric($b)) {
+    fwrite(STDERR, "$b should be a number.\n");
+    return false;
+    exit(1);
+  }
+}
+
+function math($a, $b, $divide = false) {
+  $validated = validate($a, $b);
+  if($validated == true) {
+    echo add($a, $b), PHP_EOL;
+    echo subtract($a, $b), PHP_EOL;
+    echo multiply($a, $b), PHP_EOL;
+    echo divide($a, $b), PHP_EOL;
+    echo modulus($a, $b), PHP_EOL;
+  } elseif($validated == true && $divide == true) {
+    echo "yes";
+  } else {
+    echo "Could not process your arguments.\n";
+  }
+}
 
 function add($a, $b)
 {
@@ -27,12 +59,4 @@ function modulus($a, $b)
 {
     return $a % $b;
 }
-
-echo add($a, $b), PHP_EOL;
-echo subtract($a, $b), PHP_EOL;
-echo multiply($a, $b), PHP_EOL;
-echo divide($a, $b), PHP_EOL;
-echo modulus($a, $b), PHP_EOL;
-
-echo 2 % 5;
-// Add code to test your functions here
+echo math($a, $b);
