2ff8725bd34e4be68cb4d9b343f4ce0e6eebd4e3
Anthony <martab7.am@gmail.com>
2016-07-21 16:33:43 -0500

    Checks for partial entry for log test

--- log_test.php
+++ log_test.php
@@ -2,7 +2,7 @@
 require_once('Log.php');
 
 $log = new Log();
-$log->filename = 'cli-2016-07-11.log';
+$log->filename = 'cli';
 
 echo $log->logMessage("INFO", $log->info());
 echo $log->logMessage("ERROR", $log->error());
